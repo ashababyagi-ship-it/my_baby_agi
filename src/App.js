86df2098849afd77e9d15420b0c37da89d49e7fd
ashababyagi-ship-it <ashababyagi@gmail.com>
2026-06-26 01:50:42 +0000

    First UI setup completed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+      <div style={{ padding: '20px', textAlign: 'center' }}>
+            <h1>আমার Baby AGI প্রজেক্ট</h1>
+                  <p>এজেন্ট সেটআপের কাজ শুরু করছি!</p>
+                      </div>
+                        );
+                        }
+
+                        export default App;
+                        
